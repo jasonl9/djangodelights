1b1c939455989fcde9ec678cd4e79d6a81892454
jasonl9 <jason.leeyt@outlook.com>
2023-11-30 14:15:00 +0800

    set up some models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+  name = models.CharField(max_length=50)
+  avaliable_quantity = models.IntegerField
+  unit = models.CharField(max_length=30)
+  unit_price = models.FloatField
+
+
+class MenuItem(models.Model):
+  name = models.CharField(max_length=100)
+  price = models.FloatField
+
+class RecipeRequirement(models.Model):
+  pass
+
+class Order(models.Model):
+  timestamp = models.DateTimeField
+
+class Purchase(models.Model):
+  pass
