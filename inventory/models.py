058f8fe0e1a38c0c546e75bcba4d3798b42b9d81
jasonl9 <jason.leeyt@outlook.com>
2023-12-03 11:33:03 +0800

    added __str__ function to models to display proper names on admin site

--- inventory/models.py
+++ inventory/models.py
@@ -7,23 +7,38 @@ class Ingredient(models.Model):
   unit = models.CharField(max_length=30)
   unit_price = models.FloatField()
 
-def get_absolute_url(self):
-  return "ingredient"
+  def get_absolute_url(self):
+    return "ingredient"
+
+  def __str__(self):
+    return self.name
 
 
 class MenuItem(models.Model):
   name = models.CharField(max_length=100)
   price = models.FloatField()
 
+  def __str__(self):
+    return self.name
+
 class RecipeRequirement(models.Model):
   menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
   ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
   quantity = models.FloatField()
 
+  def __str__(self):
+    return "Recipe Requirement #" + str(self.pk) + " " + str(self.menuitem) + " - " + str(self.ingredient)
+
 class Order(models.Model):
   timestamp = models.DateTimeField()
+
+  def __str__(self):
+    return "Order #" + str(self.pk)
 
 class Purchase(models.Model):
   menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
   quantity = models.IntegerField()
   order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return "Purchase #" + str(self.pk)
